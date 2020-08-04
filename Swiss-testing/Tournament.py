2e314cb355527581595bac32040ef40dec67e986
Jeff <jeffgp93@gmail.com>
2020-08-04 16:54:03 -0400

    Edge weight added

--- Swiss-testing/Tournament.py
+++ Swiss-testing/Tournament.py
@@ -1,5 +1,6 @@
 import Player
 import networkx as nx
+import random
 
 class Tournament():
 
@@ -12,6 +13,7 @@ class Tournament():
         self._unpaired_groups = []
         self._next_unpaired_group = 0 #score integer
         self._pair_lowest_next = False
+        self.round_paired = False
 
     
     def find_pairing_groups(self):
@@ -27,7 +29,7 @@ class Tournament():
                 self._score_groups[player.score] = [player]
         
         self._unpaired_groups.sort(reverse=True)
-        self._next_unpaired_group = self._unpaired_groups.pop(0)
+        self.setup_next_network()
         
     
     def add_player(self, player):
@@ -39,17 +41,87 @@ class Tournament():
             else:
                 self.player_list.append(player)
     
-    def construct_network(self):
-        score_group = self._next_unpaired_group
+    def construct_network(self,algorithm="random_swiss",iterate=True):
+        active_group = self._score_groups[self._next_unpaired_group]
         new_graph = nx.Graph()
-        for player in self._score_groups[score_group]:
+
+        #Sort by SoS then Score to deal with floaters
+        active_group.sort(key= lambda player: player.sos, reverse=True)
+        active_group.sort(key = lambda player: player.score, reverse=True)
+
+        for player in active_group:
             new_graph.add_node(player)
+
+
+        for i in range(len(active_group)):
+            for j in range(len(active_group)):
+                if i >= j:
+                    pass
+                else:
+                    edge_weight = self.algorithms[algorithm](self,active_group, i, j)
+                    new_graph.add_edge(active_group[i],active_group[j],weight = edge_weight)
+
         self._active_pairing_graph = new_graph
         
+        if iterate:  
+            self.setup_next_network()
+    
+    
+    def setup_next_network(self): 
+        try:       
+            if self._pair_lowest_next:
+                self._next_unpaired_group = self._unpaired_groups.pop(-1)
+            else:
+                self._next_unpaired_group = self._unpaired_groups.pop(0)
+        except IndexError:
+            self.round_paired = True
         self._pair_lowest_next = not self._pair_lowest_next
-        if self._pair_lowest_next:
-            self._next_unpaired_group = self._unpaired_groups.pop(-1)
+    
+    def random_Swiss_weights(self,active_group,player_one_index, player_two_index):
+        player_one = active_group[player_one_index]
+        player_two = active_group[player_two_index]
+
+        if player_one.id in player_two.opponent_list:
+            return 0
+
+        old_floater_penalty = self.compute_old_floater_penalty(player_one, player_two, player_one_index, player_two_index)
+        
+        
+        new_floater_penalty = 10 * (player_one_index + player_two_index)
+
+        side_penalty = self.compute_side_penalty(player_one, player_two)
+
+        random_weight = random.randint(0,10)
+
+        return 10000-(old_floater_penalty+new_floater_penalty+side_penalty+random_weight)
+
+
+        
+
+    def high_low_swiss_weights(self,active_group, player_one_index, player_two_index):
+        pass
+
+    def halfs_swiss_weights(self,active_group, player_one_index, player_two_index):
+        pass
+
+    def almafi_weights(self,active_group,player_one_index, player_two_index):
+        pass
+
+    algorithms = {
+        "random_swiss": random_Swiss_weights,
+        "high_low_swiss": high_low_swiss_weights,
+        "halfs_swiss": halfs_swiss_weights,
+        "almafi": almafi_weights
+    }
+
+    def compute_old_floater_penalty(self,player_one, player_two, player_one_index, player_two_index):
+        if player_one.score == player_two.score:
+            return 0
         else:
-            self._next_unpaired_group = self._unpaired_groups.pop(0)
+            return 25*(player_one.score - player_two.score)*abs(player_one_index - player_two_index)-500
     
-    
+    def compute_side_penalty(self, player_one, player_two):
+        if player_one.side_balance * player_two.side_balance > 0:
+            return 25*(8^(min(abs(player_one.side_balance),abs(player_two.side_balance))))
+        else:
+            return 0
--- Swiss-testing/TournamentTests.py
+++ Swiss-testing/TournamentTests.py
@@ -1,6 +1,7 @@
 import unittest
 import Tournament
 import Player
+import random
 
 
 def simple_pairing_setup():
@@ -37,6 +38,15 @@ class TestTournamentMethods(unittest.TestCase):
         with self.assertRaises(ValueError):
             t.add_player(Player.Player())
 
+    def test_single_group(self):
+        t = Tournament.Tournament()
+        for _ in range(2):
+            t.add_player(Player.Player())
+        # t.add_player(Player.Player())
+
+        t.find_pairing_groups()
+        self.assertEqual(t._score_groups[0],t.player_list)
+
     def test_number_of_pairing_groups(self):
         t = simple_pairing_setup()
         t.find_pairing_groups()
@@ -54,6 +64,37 @@ class TestTournamentMethods(unittest.TestCase):
         t.construct_network()
         for node in t._active_pairing_graph.nodes:
             self.assertIn(node.id, [player.id for player in t.player_list if player.score == 1])
+    
+    def test_pairing_flip(self):
+        player_list = []
+        for i in range(3):
+            player_list.append(Player.Player())
+            player_list[i].score = i
+        t = Tournament.Tournament()
+        for player in player_list:
+            t.add_player(player)
+
+        t.find_pairing_groups()
+        self.assertEqual(t._next_unpaired_group, 2)
+        self.assertTrue(t._pair_lowest_next)
+
+        t.setup_next_network()
+        self.assertEqual(t._next_unpaired_group, 0)
+        self.assertFalse(t._pair_lowest_next)
+
+        t.setup_next_network()
+        self.assertEqual(t._next_unpaired_group, 1)
+        self.assertTrue(t._pair_lowest_next)
+
+    def test_random_weight_calculation(self):
+        t = Tournament.Tournament()
+        for _ in range(2):
+            t.add_player(Player.Player())
+        t.find_pairing_groups()
+        for _ in range(100):
+            t.construct_network(iterate=False)
+            self.assertGreater(t._active_pairing_graph[t.player_list[0]][t.player_list[1]]['weight'], 10000 - 30)
+            self.assertLess(t._active_pairing_graph[t.player_list[0]][t.player_list[1]]['weight'], 10000)
 
 
 if __name__ == '__main__':
